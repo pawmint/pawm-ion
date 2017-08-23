a4bde26e13a4c29a52ab3ecedabc950ac14ef187
Martin Kodys <mkodys@gmail.com>
2017-08-23 12:14:23 +0800

    Server change for icost
    TODO: configuration mechanism

--- www/js/app.js
+++ www/js/app.js
@@ -147,7 +147,7 @@ var app = angular.module('pawm', ['ionic', 'login_Ubismart', 'communicator_Ubism
   // Show UbiSMART interface view of My Services
   $scope.startUbiSmartWeb = function() {
     // TODO: Verify the localStorage.authToken is VALID!
-    $window.open('https://touch-sg.ubismart.org/service/appBroker?action=authByToken&authToken=' + encodeURIComponent(localStorage.authToken),'_system','location=no');
+    $window.open('https://icost.ubismart.org/service/appBroker?action=authByToken&authToken=' + encodeURIComponent(localStorage.authToken),'_system','location=no');
   };
 }])
 .controller("suggestController", ['$scope', 'SystemInfo', 'AuthenticationService', 'CommunicatorService', '$ionicPopup', function($scope, SystemInfo, AuthenticationService, CommunicatorService, $ionicPopup) {
